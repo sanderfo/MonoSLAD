00d865cdd0fa2b46d20bf4f8f2a2347013146de9
sander <sandef@uio.no>
2023-01-16 23:58:24 +0100

    need to fix launch file

--- src/autonomous_landing/src/dense_verifier.cpp
+++ src/autonomous_landing/src/dense_verifier.cpp
@@ -1,7 +1,10 @@
 #include "ros/ros.h"
+#include <tf/transform_broadcaster.h>
 #include <Eigen/Dense>
+#include <eigen_conversions/eigen_msg.h>
 #include <image_transport/image_transport.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <opencv2/core/mat.hpp>
@@ -20,14 +23,17 @@ class DenseVerifier
         n_.getParam("safetyZoneSize", safetyZoneSize);
         n_.getParam("sweepN", sweepN);
         n_.getParam("matching_threshold", matching_threshold);
+        n_.getParam("distance_threshold_squared", distance_threshold_squared);
         
-
         ROS_INFO("%f %d %f", safetyZoneSize, sweepN, matching_threshold);
 
         dense_pub_ =
             n_.advertise<PointCloud>("autonomous_landing/dense_pointcloud", 1);
         depth_pub_ =
             it.advertise("autonomous_landing/depth_image", 1);
+
+        cam_pose_pub_ =
+            n_.advertise<geometry_msgs::PoseStamped>("autonomous_landing/cam_pose", 1);
 
         imgsub_ = it.subscribe("/cam0/image_raw", 1, &DenseVerifier::imageCallback, this);
         posesub_ = n_.subscribe("/svo/backend_pose_imu", 2, &DenseVerifier::poseCallback, this);
@@ -47,18 +53,16 @@ class DenseVerifier
 
     void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
         
-        ROS_INFO("image timestamp %d %d", msg->header.stamp.sec, msg->header.stamp.nsec);
         image_candidate_vector.push_back(msg);
     }
 
     void poseCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg) {
         
-        ROS_INFO("pose timestamp %d %d", msg->header.stamp.sec, msg->header.stamp.nsec);
-        double distanceSquared = 
+        double distance_squared = 
             std::pow(msg->pose.pose.position.x - current_pose.position.x, 2)
           + std::pow(msg->pose.pose.position.y - current_pose.position.y, 2)
           + std::pow(msg->pose.pose.position.z - current_pose.position.z, 2);
-        if(distanceSquared > 0.1) {
+        if(distance_squared > distance_threshold_squared) {
             pose_array[pose_counter] = msg->pose.pose;
             current_pose = msg->pose.pose;
 
@@ -93,7 +97,9 @@ class DenseVerifier
     ros::NodeHandle n_ = *(new ros::NodeHandle("~"));
     image_transport::ImageTransport it = *(new image_transport::ImageTransport(n_));
     ros::Publisher dense_pub_;
+    ros::Publisher cam_pose_pub_;
     image_transport::Publisher depth_pub_;
+    tf::TransformBroadcaster br;
     image_transport::Subscriber imgsub_;
     ros::Subscriber posesub_;
     ros::Subscriber landingsub_;
@@ -101,6 +107,7 @@ class DenseVerifier
     float safetyZoneSize;
     int sweepN;
     float matching_threshold;
+    float distance_threshold_squared;
     Eigen::Matrix3d K;
 
     std::vector<sensor_msgs::ImageConstPtr> image_candidate_vector;
@@ -115,8 +122,9 @@ class DenseVerifier
         std::vector<PSL::CameraMatrix<double>> cameras(sweepN);
         for (int c = 0; c < sweepN; c++) {
           auto cam_R = util::poseToRotation(pose_array[c]);
-          cam_R.transposeInPlace();
-          auto cam_T = -cam_R * util::poseToPosition(pose_array[c]);
+          //cam_R.transposeInPlace();
+          //auto cam_T = -cam_R * util::poseToPosition(pose_array[c]);
+          auto cam_T = util::poseToPosition(pose_array[c]);
           cameras[c].setKRT(K, cam_R,
                             cam_T);
         };
@@ -125,7 +133,6 @@ class DenseVerifier
         double avg_distance = 0; // in order to find a good z-range
         int num_distances = 0;
 
-        Eigen::Vector3d ref_coordinate = cameras[0].getC();
         for (int i = 0; i < sweepN - 1; i++) {
           for (int j = i + 1; j < sweepN; j++) {
             avg_distance += (cameras[i].getC() - cameras[j].getC()).norm();
@@ -180,6 +187,33 @@ class DenseVerifier
         PointCloud::Ptr dense_pointcloud (new PointCloud);
         dense_pointcloud->is_dense = true;
 
+        tf::Transform transform;
+
+        auto R = depth_map.getCam().getR();
+        auto T = depth_map.getCam().getT();
+
+        tf::Vector3 t(T[0],T[1],T[2]);
+
+        Eigen::Quaterniond eigen_q(R);
+        tf::Quaternion q(eigen_q.x(),eigen_q.y(),eigen_q.z(),eigen_q.w());
+        
+
+        transform.setOrigin(t);
+        transform.setRotation(q);
+
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "dense_cam"));
+
+        Eigen::Affine3d pose_cam;
+        pose_cam.setIdentity();
+        pose_cam.pretranslate(T);
+        pose_cam.rotate(R);
+        
+        geometry_msgs::PoseStamped pose_msg;
+        
+        tf::poseEigenToMsg(pose_cam, pose_msg.pose);
+        pose_msg.header.frame_id = std::string("world");
+        cam_pose_pub_.publish(pose_msg);
+
 
         for (int i = 0; i < width; i++) 
         {
@@ -187,8 +221,17 @@ class DenseVerifier
             {   Eigen::Vector4d eig_point = depth_map.unproject(i, j);
                 
                 
-                if (eig_point[3] > 0 && costs(i, j) < matching_threshold)
+                if (eig_point[3] > 0.5 && costs(i, j) < matching_threshold)
                 {
+                    
+
+                    eig_point[0] = (i - K(0,2))*depth_map(i,j)/K(0,0);
+                    eig_point[1] = (j - K(1,2))*depth_map(i,j)/K(1,1);
+                    eig_point[2] = depth_map(i,j);
+
+                    //eig_point = pose_cam*eig_point;
+
+                    //eig_point = depth_map.getCam().getCam2Global().inverse() * eig_point;
                     pcl::PointXYZI pcl_point;
                 
                     pcl_point.x = eig_point[0];
@@ -208,9 +251,11 @@ class DenseVerifier
         }
         sensor_msgs::ImagePtr depth_msg = depth_map_to_msg(depth_map, min_z, max_z);
         depth_pub_.publish(depth_msg);
-        dense_pointcloud->header.frame_id = std::string("world");
+        dense_pointcloud->header.frame_id = std::string("dense_cam");
         
         dense_pub_.publish(dense_pointcloud);
+
+        
     };
 
     sensor_msgs::ImagePtr depth_map_to_msg(PSL::DepthMap<float, double> dM, float min_z,
